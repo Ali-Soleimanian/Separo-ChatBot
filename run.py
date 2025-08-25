5e8faccce9aeb2fdc3b99bc283f324f6558ad156
Ali <alisoleimanian.it@gmail.com>
2025-08-25 19:07:47 +0330

    chat style created

--- run.py
+++ run.py
@@ -2,14 +2,26 @@ import streamlit as st
 from langchain_groq import ChatGroq
 from config.settings import settings
 
-st.set_page_config(page_title="Separo ChatBot", page_icon="🤖")
+st.set_page_config(page_title="Separo ChatBot", page_icon="media/bot profile.png")
 
-st.markdown(
-    "<h1 style='text-align:center;'> Welcome to Separo ChatBot</h1>",
-    unsafe_allow_html=True
-)
 
-user_input = st.text_area("")
+
+
+st.markdown("""
+<h1 style="text-align:center;">
+Welcome to <span style="
+    background: linear-gradient(to right, green 25%, red);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+            font-size:50px;
+">Separo</span> ChatBot
+</h1>
+""",
+unsafe_allow_html=True)
+
+st.write("")
+st.write("")
+
 
 llm = ChatGroq(
     model="llama3-70b-8192",
@@ -17,9 +29,9 @@ llm = ChatGroq(
     temperature=0.7,
 )
 
-if st.button("Send"):
-    if user_input.strip():
-        response = llm.invoke(user_input)
-        st.success(response.content)
-    else:
-        st.warning("pleas type somthing")
+
+if prompt := st.chat_input("Say something"):
+    response = llm.invoke(prompt)
+    st.chat_message("user", avatar="media/user profile.png").write(prompt)
+    st.chat_message("assistant", avatar="media/bot profile.png").write(response.content)
+
